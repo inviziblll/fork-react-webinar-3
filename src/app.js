07724d66a94751c289221b97cca3618406068f92
alex.burov <alex.burov@bitrix24.ru>
2024-09-12 14:41:57 +0300

    -- selected, quantity, unic code --

--- src/app.js
+++ src/app.js
@@ -27,7 +27,9 @@ function App({ store }) {
                 onClick={() => store.selectItem(item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
+                <div className="Item-title">
+                  {item.title} {item.quantity ? 'Выделяли '+item.quantity+' раз' : '' }
+                </div>
                 <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
