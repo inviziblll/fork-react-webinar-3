5efd986ae72a61aa2f16e5f3a3e6a28f8d4a7054
alex.burov <alex.burov@bitrix24.ru>
2024-09-20 00:40:48 +0300

    -- update dist --

--- src/store.js
+++ src/store.js
@@ -52,7 +52,7 @@ class Store {
       let product = this.state.list.find(item => item.code === code);
       let cartProduct = this.state.cart.find(item => item.code === code); 
 
-      if (cartProduct) { // если товар уже есть в корзине учеличиваем его количество и цену
+      if (cartProduct) { // если товар уже есть в корзине увеличиваем его количество и цену
         
           let newCart = this.state.cart.map(item =>
             item.code === code
